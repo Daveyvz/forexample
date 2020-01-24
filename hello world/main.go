75e509021aaa18a8fdfab58ae685f6cff0a893a3
Davey <davey_vanz@live.nl>
2020-01-24 12:14:29 +0100

    atomic

--- hello world/main.go
+++ hello world/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("CPU", runtime.NumCPU())
+	fmt.Println("GoR", runtime.NumGoroutine())
+	var counter int64
+
+	var wg sync.WaitGroup
+	//var mu sync.Mutex
+
+	const gs = 100
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			//mu.Lock()
+			atomic.AddInt64(&counter, 1)
+			fmt.Println("Counter\t", atomic.LoadInt64(&counter))
+			runtime.Gosched() // allows the cpu to run something else
+			//mu.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("GoR", runtime.NumGoroutine())
+
+	fmt.Println(counter)
 }
